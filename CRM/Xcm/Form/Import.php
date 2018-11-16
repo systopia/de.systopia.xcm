1a1a8c47b0db71fb5c2d848b834057ca8bc51983
B. Endres <endres@systopia.de>
2018-11-16 10:36:14 +0100

    profile refactoring (#30) WIP

--- CRM/Xcm/Form/Import.php
+++ CRM/Xcm/Form/Import.php
@@ -25,14 +25,14 @@ class CRM_Xcm_Form_Import extends CRM_Core_Form {
   public function buildQuickForm() {
     $config = CRM_Core_Config::singleton();
 
-    $uploadFileSize = CRM_Utils_Number::formatUnitSize(
+    $uploadFileSize = self::formatUnitSize(
       $config->maxFileSize . 'm',
       TRUE
     );
     // Fetch uploadFileSize from php_ini when $config->maxFileSize is set to
     // "no limit".
     if (empty($uploadFileSize)) {
-      $uploadFileSize = CRM_Utils_Number::formatUnitSize(
+      $uploadFileSize = self::formatUnitSize(
         ini_get('upload_max_filesize'),
         TRUE
       );
@@ -68,6 +68,12 @@ class CRM_Xcm_Form_Import extends CRM_Core_Form {
       ts('A valid file must be uploaded.'),
       'uploadedfile'
     );
+
+    $this->addElement('select',
+        'profile_name',
+        ts('Use Configuration Profile', array('domain' => 'de.systopia.xcm')),
+        CRM_Xcm_Configuration::getProfileList(),
+        array('class' => 'crm-select2 huge'));
 
     $this->addButtons(array(
       array(
@@ -161,7 +167,9 @@ class CRM_Xcm_Form_Import extends CRM_Core_Form {
             }
           }
         }
-        $result = civicrm_api3('Contact', 'getorcreate', array_combine($firstrow, $record));
+        $contact_data = array_combine($firstrow, $record);
+        $contact_data['xcm_profile'] = $values['profile_name'];
+        $result = civicrm_api3('Contact', 'getorcreate', $contact_data);
         if ($result['is_error']) {
           throw new Exception($result['error_message']);
         }
@@ -213,6 +221,51 @@ class CRM_Xcm_Form_Import extends CRM_Core_Form {
       }
     }
     return $elementNames;
+  }
+
+  /**
+   * Copied from CiviCRM 4.7+
+   *
+   * @param string $size
+   * @param bool $checkForPostMax
+   *
+   * @return int
+   */
+  public static function formatUnitSize($size, $checkForPostMax = FALSE) {
+    // if the method exists in CiviCRM -> use that!
+    if (method_exists('CRM_Utils_Number', 'formatUnitSize')) {
+      return CRM_Utils_Number::formatUnitSize();
+    }
+
+    // doesn't exist -> we're probably on CiviCRM 4.6
+    if ($size) {
+      $last = strtolower($size{strlen($size) - 1});
+      $size = (int) $size;
+      switch ($last) {
+        // The 'G' modifier is available since PHP 5.1.0
+
+        case 'g':
+          $size *= 1024;
+        case 'm':
+          $size *= 1024;
+        case 'k':
+          $size *= 1024;
+      }
+
+      if ($checkForPostMax) {
+        $maxImportFileSize = self::formatUnitSize(ini_get('upload_max_filesize'));
+        $postMaxSize = self::formatUnitSize(ini_get('post_max_size'));
+        if ($maxImportFileSize > $postMaxSize && $postMaxSize == $size) {
+          CRM_Core_Session::setStatus(ts("Note: Upload max filesize ('upload_max_filesize') should not exceed Post max size ('post_max_size') as defined in PHP.ini, please check with your system administrator."), ts("Warning"), "alert");
+        }
+        // respect php.ini upload_max_filesize
+        if ($size > $maxImportFileSize && $size !== $postMaxSize) {
+          $size = $maxImportFileSize;
+          CRM_Core_Session::setStatus(ts("Note: Please verify your configuration for Maximum File Size (in MB) <a href='%1'>Administrator >> System Settings >> Misc</a>. It should support 'upload_max_size' as defined in PHP.ini.Please check with your system administrator.", array(1 => CRM_Utils_System::url('civicrm/admin/setting/misc', 'reset=1'))), ts("Warning"), "alert");
+        }
+      }
+      return $size;
+    }
   }
 
 }
