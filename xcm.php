1c33e1e3b6b532a568ab2d53cd7f209d4a06bfa9
Anna <bugey@systopia.de>
2022-11-15 10:37:57 +0100

    [98] civix upgrade

--- xcm.php
+++ xcm.php
@@ -40,17 +40,6 @@ function xcm_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function xcm_civicrm_xmlMenu(&$files) {
-  _xcm_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -104,56 +93,6 @@ function xcm_civicrm_disable() {
  */
 function xcm_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _xcm_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function xcm_civicrm_managed(&$entities) {
-  _xcm_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function xcm_civicrm_caseTypes(&$caseTypes) {
-  _xcm_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function xcm_civicrm_angularModules(&$angularModules) {
-_xcm_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function xcm_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _xcm_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -255,4 +194,22 @@ function _xcm_menu_exists(&$menu, $path) {
     }
   }
   return $found;
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function xcm_civicrm_postInstall() {
+  _xcm_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function xcm_civicrm_entityTypes(&$entityTypes) {
+  _xcm_civix_civicrm_entityTypes($entityTypes);
 }
