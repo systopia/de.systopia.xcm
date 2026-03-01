45af77bbf2d00e0dec894d9168629c3493255532
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2026-03-01 20:26:41 +0100

    Add Only to the name

--- CRM/Xcm/Matcher/ExternalIdentifierOnlyMatcher.php
+++ CRM/Xcm/Matcher/ExternalIdentifierOnlyMatcher.php
@@ -1,6 +1,6 @@
 <?php
 
-class CRM_Xcm_Matcher_ExternalIdentifierMatcher extends CRM_Xcm_Matcher_SingleAttributeMatcher {
+class CRM_Xcm_Matcher_ExternalIdentifierOnlyMatcher extends CRM_Xcm_Matcher_SingleAttributeMatcher {
 
   public function __construct() {
     parent::__construct('external_identifier', 'Contact', ['external_identifier']);
